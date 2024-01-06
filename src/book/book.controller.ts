6ec4b7c793457581217e2c7632bebe28907e55f0
timonjagi <timothynjihia@gmail.com>
2024-01-06 14:05:10 +0300

    Add search and pagination

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,51 +1,50 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 import { BookService } from './book.service';
 import { Book } from './schemas/book.schema';
 import { CreateBookDTO } from './dto/create-book.dto';
 import { UpdateBookDTO } from './dto/update-book.dto';
+import { Query as ExpressQuery } from 'express-serve-static-core';
 
 @Controller('books')
 export class BookController {
   constructor(private bookService: BookService) { }
 
   @Get()
-  async getAllBooks(): Promise<Book[]> {
-    return this.bookService.findAll();
+  async getAllBooks(@Query() query: ExpressQuery): Promise<Book[]> {
+    console.log(query)
+
+    return this.bookService.findAll(query);
   }
 
   @Get(':id')
-  async getBook(
-    @Param('id')
-    id: string
-  ): Promise<Book> {
+  async getBook(@Param('id') id: string): Promise<Book> {
     return this.bookService.findById(id);
   }
 
   @Post()
-  async createBook(
-    @Body()
-    book: CreateBookDTO,
-  ): Promise<Book> {
+  async createBook(@Body() book: CreateBookDTO): Promise<Book> {
     return this.bookService.create(book);
   }
 
   @Put(':id')
   async updateBook(
-    @Param('id')
-    id: string,
-    @Body()
-    book: UpdateBookDTO,
+    @Param('id') id: string,
+    @Body() book: UpdateBookDTO,
   ): Promise<Book> {
     return this.bookService.updateById(id, book);
   }
 
-
   @Delete(':id')
-  async deleteBook(
-    @Param('id')
-    id: string,
-  ): Promise<Book> {
+  async deleteBook(@Param('id') id: string): Promise<Book> {
     return this.bookService.deleteById(id);
   }
-
 }
